62fce5cc3e1ed1fc9bb15b0206ee2ebae9b2f677
Test acc <test@krypton.(none)>
2013-07-10 03:06:29 -0700

    comment on append to auth_key file

--- ssh.sh
+++ ssh.sh
@@ -9,5 +9,8 @@ auth=https://raw.github.com/sir-ragna/setup/master/authorized_keys
 cd $HOME
 mkdir --mode=700 -p .ssh
 cd .ssh/
-wget -qO- $auth >> authorized_keys
+wget -qO- $auth >> authorized_keys # appends does not overwrite
+                                   # this should change if auth
+                                   # file is guaranteed to be 
+                                   # up to date.
 
